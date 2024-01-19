f453a066e9332be5062a70625654384e6703a79c
xhayper <46065349+xhayper@users.noreply.github.com>
2024-01-19 11:05:19 +0700

    first commit

---
+++ NoFollowerLevelLimit/Patches/FollowerBrainInfoPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace NoFollowerLevelLimit.Patches;
+
+[HarmonyPatch]
+public class FollowerBrainInfoPatches
+{
+    [HarmonyPatch(typeof(FollowerBrainInfo), nameof(FollowerBrainInfo.MaxLevelReached), MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool FollowerBrainInfo_MaxLevelReached_getter(ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(FollowerBrainInfo), nameof(FollowerBrainInfo.MaxLevelReached), MethodType.Setter)]
+    [HarmonyPrefix]
+    private static bool FollowerBrainInfo_MaxLevelReached_setter(FollowerBrainInfo __instance)
+    {
+        __instance._info.MaxLevelReached = false;
+        return false;
+    }
+}
---
+++ NoFollowerLevelLimit/Patches/FollowerBrainPatches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace NoFollowerLevelLimit.Patches;
+
+[HarmonyPatch]
+public class FollowerBrainPatches
+{
+    [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.IsMaxLevel))]
+    [HarmonyPrefix]
+    private static bool FollowerBrain_IsMaxLevel(ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.OnMaxLevelReached))]
+    [HarmonyPrefix]
+    private static bool FollowerBrain_OnMaxLevelReached()
+    {
+        return false;
+    }
+}
