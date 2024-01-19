b7deab61bfdde2777414a44c6ded007284713f38
xhayper <46065349+xhayper@users.noreply.github.com>
2024-01-19 14:13:57 +0700

    feat: add more patches

--- NoFollowerLevelLimit/Patches/FollowerBrainPatches.cs
+++ NoFollowerLevelLimit/Patches/FollowerBrainPatches.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Reflection.Emit;
+using HarmonyLib;
 
 namespace NoFollowerLevelLimit.Patches;
 
@@ -12,11 +13,108 @@ public class FollowerBrainPatches
         __result = false;
         return false;
     }
-    
+
     [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.OnMaxLevelReached))]
     [HarmonyPrefix]
     private static bool FollowerBrain_OnMaxLevelReached()
     {
+        return false;
+    }
+
+    /*
+     Old instruction
+     IL_0008: ldarg.0      // this
+     IL_0009: ldfld        class FollowerBrainStats FollowerBrain::Stats
+     IL_000e: callvirt     instance bool FollowerBrainStats::get_HasLevelledUp()
+     IL_0013: brtrue.s     IL_0047
+
+     IL_0015: call         class DataManager DataManager::get_Instance()
+     IL_001a: ldfld        bool DataManager::ShowLoyaltyBars
+     IL_001f: brfalse.s    IL_0047
+     IL_0021: ldarg.0      // this
+     IL_0022: ldfld        class FollowerBrainInfo FollowerBrain::Info
+     IL_0027: callvirt     instance int32 FollowerBrainInfo::get_XPLevel()
+     IL_002c: ldc.i4.s     10 // 0x0a
+     IL_002e: bge.s        IL_0047
+
+     New instruction
+     IL_0008: ldarg.0      // this
+     IL_0009: ldfld        class FollowerBrainStats FollowerBrain::Stats
+     IL_000e: callvirt     instance bool FollowerBrainStats::get_HasLevelledUp()
+     IL_0013: brtrue.s     IL_0047
+
+     IL_0015: call         class DataManager DataManager::get_Instance()
+     IL_001a: ldfld        bool DataManager::ShowLoyaltyBars
+     IL_001f: brfalse.s    IL_0047
+     IL_0021: nop
+     IL_0022: nop
+     IL_0027: nop
+     IL_002c: nop
+     IL_002e: nop
+     */
+    [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.AddAdoration))]
+    [HarmonyPatch([typeof(Follower), typeof(FollowerBrain.AdorationActions), typeof(Action)])]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> FollowerBrain_AddAdoration(IEnumerable<CodeInstruction> instructions)
+    {
+        var codes = new List<CodeInstruction>(instructions);
+
+        var found = false;
+        var codeIndex = 0;
+
+        // TODO: Add operand check
+        for (var i = 0; i < codes.Count; i++)
+        {
+            // this.Stats.HasLevelledUp
+            if (codes[i].opcode != OpCodes.Ldarg_0) continue;
+            if (codes[i + 1].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 2].opcode != OpCodes.Callvirt) continue;
+            if (codes[i + 3].opcode != OpCodes.Brtrue_S) continue;
+            
+            // !DataManager.Instance.ShowLoyaltyBars
+            if (codes[i + 4].opcode != OpCodes.Call) continue;
+            if (codes[i + 5].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 6].opcode != OpCodes.Brfalse_S) continue;
+            
+            // this.Info.XPLevel >= 10
+            if (codes[i + 7].opcode != OpCodes.Ldarg_0) continue;
+            if (codes[i + 8].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 9].opcode != OpCodes.Callvirt) continue;
+            if (codes[i + 10].opcode != OpCodes.Ldc_I4_S && (int)codes[i + 3].operand == 10) continue;
+            if (codes[i + 11].opcode != OpCodes.Bge_S) continue;
+
+            found = true;
+            codeIndex = i;
+
+            break;
+        }
+
+        if (!found) return codes.AsEnumerable();
+
+        codes[codeIndex + 7] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 8] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 9] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 10] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 11] = new CodeInstruction(OpCodes.Nop);
+
+        return codes.AsEnumerable();
+    }
+
+    [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.GetWillLevelUp))]
+    [HarmonyPrefix]
+    private static bool FollowerBrain_GetWillLevelUp(FollowerBrain __instance, ref bool __result,
+        ref FollowerBrain.AdorationActions Action)
+    {
+        __result = (double)__instance.Stats.Adoration + __instance.GetAddorationToAdd(Action) >=
+                   __instance.Stats.MAX_ADORATION;
+        return false;
+    }
+
+    [HarmonyPatch(typeof(FollowerBrain), nameof(FollowerBrain.CanLevelUp))]
+    [HarmonyPrefix]
+    private static bool FollowerBrain_GetWillLevelUp(FollowerBrain __instance, ref bool __result)
+    {
+        __result = (double)__instance.Stats.Adoration >= __instance.Stats.MAX_ADORATION;
         return false;
     }
 }
---
+++ NoFollowerLevelLimit/Patches/FollowerInformationBoxPatches.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace NoFollowerLevelLimit.Patches;
+
+[HarmonyPatch]
+public class FollowerInformationBoxPatches
+{
+    /*
+    Old instruction
+    IL_04c4: ldarg.0      // this
+    IL_04c5: ldfld        class [UnityEngine.CoreModule]UnityEngine.GameObject FollowerInformationBox::_adorationContainer
+    IL_04ca: callvirt     instance class [UnityEngine.CoreModule]UnityEngine.GameObject [UnityEngine.CoreModule]UnityEngine.GameObject::get_gameObject()
+    IL_04cf: ldarg.0      // this
+    IL_04d0: ldfld        class FollowerBrain FollowerInformationBox::followBrain
+    IL_04d5: ldfld        class FollowerBrainInfo FollowerBrain::Info
+    IL_04da: callvirt     instance int32 FollowerBrainInfo::get_XPLevel()
+    IL_04df: ldc.i4.s     10 // 0x0a
+    IL_04e1: clt
+    IL_04e3: callvirt     instance void [UnityEngine.CoreModule]UnityEngine.GameObject::SetActive(bool)
+
+    New instruction
+    IL_04c4: ldarg.0      // this
+    IL_04c5: ldfld        class [UnityEngine.CoreModule]UnityEngine.GameObject FollowerInformationBox::_adorationContainer
+    IL_04ca: callvirt     instance class [UnityEngine.CoreModule]UnityEngine.GameObject [UnityEngine.CoreModule]UnityEngine.GameObject::get_gameObject()
+    IL_04cf: ldc.i4.1
+    IL_04d0: nop
+    IL_04d5: nop
+    IL_04da: nop
+    IL_04df: nop
+    IL_04e1: nop
+    IL_04e3: callvirt     instance void [UnityEngine.CoreModule]UnityEngine.GameObject::SetActive(bool)
+
+    */
+    [HarmonyPatch(typeof(FollowerInformationBox), nameof(FollowerInformationBox.ConfigureImpl))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> FollowerInformationBox_ConfigureImpl(IEnumerable<CodeInstruction> instructions)
+    {
+        var codes = new List<CodeInstruction>(instructions);
+
+        var found = false;
+        var codeIndex = 0;
+        
+        // TODO: fill in operand
+        for (var i = 0; i < codes.Count; i++)
+        {
+            // get adorationContainer
+            if (codes[i].opcode != OpCodes.Ldarg_0) continue;
+            if (codes[i + 1].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 2].opcode != OpCodes.Callvirt) continue;
+
+            // this.followBrain.Info.XPLevel < 10
+            if (codes[i + 3].opcode != OpCodes.Ldarg_0) continue;
+            if (codes[i + 4].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 5].opcode != OpCodes.Ldfld) continue;
+            if (codes[i + 6].opcode != OpCodes.Callvirt) continue;
+            if (codes[i + 7].opcode != OpCodes.Ldc_I4_S && (int)codes[i + 7].operand == 10) continue;
+            if (codes[i + 8].opcode != OpCodes.Clt) continue;
+
+            // set active
+            if (codes[i + 9].opcode != OpCodes.Callvirt) continue;
+
+            found = true;
+            codeIndex = i;
+
+            break;
+        }
+
+        if (!found) return codes.AsEnumerable();
+        
+        codes[codeIndex + 3] = new CodeInstruction(OpCodes.Ldc_I4_1);
+        codes[codeIndex + 4] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 5] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 6] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 7] = new CodeInstruction(OpCodes.Nop);
+        codes[codeIndex + 8] = new CodeInstruction(OpCodes.Nop);
+
+        return codes.AsEnumerable();
+    }
+}
