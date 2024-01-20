79178ab4bd1557fa2dd3f74619967ee3b2582efa
xhayper <46065349+xhayper@users.noreply.github.com>
2024-01-20 23:21:12 +0700

    feat: add more patches

---
+++ NoFollowerLevelLimit/Patches/RitualInfoCardPatches.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+using Lamb.UI.Rituals;
+
+namespace NoFollowerLevelLimit.Patches;
+
+public class RitualInfoCardPatches
+{
+    [HarmonyPatch(typeof(RitualInfoCard), nameof(RitualInfoCard.Configure))]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> RitualItem_Configure(IEnumerable<CodeInstruction> instructions)
+    {
+        return new CodeMatcher(instructions)
+            .MatchForward(false,
+                new CodeMatch(i => i.opcode == OpCodes.Ldloc_S),
+                new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(FollowerBrain), "Info")),
+                new CodeMatch(OpCodes.Callvirt,
+                    AccessTools.PropertyGetter(typeof(FollowerBrainInfo), "XPLevel")),
+                new CodeMatch(i => i.opcode == OpCodes.Ldc_I4_S && ((sbyte)10).Equals(i.operand)) //,
+                // new CodeMatch(i => i.opcode == OpCodes.Blt_S)
+            )
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetAndAdvance(OpCodes.Nop, null)
+            .SetAndAdvance(OpCodes.Nop, null)
+            .InstructionEnumeration();
+    }
+}
